30c9dd0ecbbea5d07990ee13fd3f015888b12eb5
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-11 05:34:52 +0300

    nvim markdown view

---
+++ .config/nvim/lua/plugins/markdown-view.lua
@@ -0,0 +1,19 @@
+function config()
+  local presets = require("markview.presets");
+
+  require("markview").setup({
+    checkboxes = presets.checkboxes.nerd,
+    headings = presets.headings.glow
+  });
+end
+
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = true,
+  ft = "markdown", -- If you decide to lazy-load anyway
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons"
+  }
+}
