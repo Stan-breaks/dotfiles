af55874823a6e4111df3b7191c21921b1930e1aa
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-25 10:46:35 +0300

    nvim
    signs

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -7,12 +7,20 @@ local config = function()
 
   local lspconfig = require("lspconfig")
 
-  local signs = { Error = "❌", Warn = "⚠️", Hint = "💡", Info = "ℹ️" }
-
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-  end
+  vim.diagnostic.config({
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "❌",
+        [vim.diagnostic.severity.WARN] = "⚠️",
+        [vim.diagnostic.severity.HINT] = "💡",
+        [vim.diagnostic.severity.INFO] = "ℹ️",
+      },
+    },
+    virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+  })
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
