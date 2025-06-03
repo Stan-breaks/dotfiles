74b951c9c9e717e14b27d0c811c726dc9756f98d
Stan-breaks <stanleymwendwa03@gmail.com>
2025-06-03 23:25:31 +0300

    nvim
    clean up

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -39,45 +39,6 @@ return {
       },
     },
   },
-  -- trouble
-  {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    event = "VeryLazy",
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
   -- gitsigns
   {
     "lewis6991/gitsigns.nvim",
@@ -211,12 +172,10 @@ return {
     config = true,
     opts = ...
   }, {
-  'augmentcode/augment.vim',
-  event = "VeryLazy",
-  cmd = "Augment",
-}, {
   "LunarVim/bigfile.nvim",
   event = "BufReadPre",
   opts = {}
+}, {
+  "tpope/vim-obsession"
 }
 }
