c955298019d458327595258cdda120d8e4a71c2a
Stan-breaks <stanleymwendwa03@gmail.com>
2026-01-28 21:51:38 +0300

    feat: mango

---
+++ .config/mango/autostart.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+set +e
+
+
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+
+"$HOME/.config/scripts/randomWallpaper.sh" "$HOME/Pictures/wallpapers/saitama.jpeg" &
+
+waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css &
+
+"$HOME/.local/share/bin/batterynotify.sh" &
+
+dunst &
+
+wl-paste --type text --watch cliphist store 
+wl-paste --type image --watch cliphist store
+
+wlsunset -T 3501 -t 3500 &
+
+notify-send -i /home/Stanbreaks/Pictures/logo.png -t 14000 "Hey Stanley. Glad you're back." "Let’s get to work." &
+
+
+export PATH="$PATH:$HOME/.local/bin"
+
+
+# Permission authentication
+/usr/lib/xfce-polkit/xfce-polkit &
+
+
+
+
+
+
+
+
+
+
+
+
+#
