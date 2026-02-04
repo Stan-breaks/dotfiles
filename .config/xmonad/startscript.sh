fc24762255d1c6d08b4d6a6f7aa56421118fad7d
Stan-breaks <stanleymwendwa03@gmail.com>
2026-02-04 14:15:13 +0300

    feat: xmobar

--- .config/xmonad/startscript.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-feh --bg-fill --no-fehbg ~/Pictures/wallpapers/wallhaven-6llxkw.jpg &
-xsetroot -cursor_name xC_left_ptr
-dunst &
-notify-send -i /home/Stanbreaks/Pictures/logo.png -t 14000 "Hey Stanley. Glad you're back." "Let’s get to work." &
-export PATH="$PATH:$HOME/.local/bin"
-
